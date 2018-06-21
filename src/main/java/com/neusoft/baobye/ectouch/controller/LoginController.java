93c4a21c6beb6530e7c820f8a5c78abf30ca291d
baobye <baobye@sina.com>
2018-06-21 13:30:18 +0800

    时间 毫秒 修改成秒

--- src/main/java/com/neusoft/baobye/ectouch/controller/LoginController.java
+++ src/main/java/com/neusoft/baobye/ectouch/controller/LoginController.java
@@ -120,16 +120,4 @@ public class LoginController {
         }
         return "error";
     }
-
-
-    @RequestMapping(value="/logout", method = RequestMethod.GET)
-    public String logoutPage (HttpServletRequest request, HttpServletResponse response) {
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        if (auth != null){
-            new SecurityContextLogoutHandler().logout(request, response, auth);
-        }
-        return "redirect:/login";//You can redirect wherever you want, but generally it's a good practice to show login screen again.
-    }
-
-
 }
