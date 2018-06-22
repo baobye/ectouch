489277fe112f7a6ba8aca3c44eed2e675bbd0026
baobye <baobye@sina.com>
2018-06-22 15:47:52 +0800

    修改商品列表 修改我的代理列表

--- src/main/java/com/neusoft/baobye/ectouch/controller/AgentController.java
+++ src/main/java/com/neusoft/baobye/ectouch/controller/AgentController.java
@@ -5,14 +5,20 @@ import com.neusoft.baobye.ectouch.entity.WapUser;
 import com.neusoft.baobye.ectouch.mapper.PriceTotalMapper;
 import com.neusoft.baobye.ectouch.mapper.WapUserMapper;
 import com.neusoft.baobye.ectouch.util.HttpClientUtil;
+import org.apache.http.HttpRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.transaction.Transactional;
 import java.io.IOException;
 import java.math.BigInteger;
@@ -33,19 +39,30 @@ public class AgentController {
      * 我的代理首页
      * @return
      */
-    @RequestMapping("/index/{userId}")
+    @RequestMapping("/index")
     @Transactional
-    public String index(@PathVariable Long userId, Model model){
-        if(userId == null || userId == 0){
-            String name = SecurityContextHolder.getContext().getAuthentication().getName();
-            WapUser user  = userMapper.findByUsername(name);
-            userId = user.getUserId();
-            //发奖
-            model.addAttribute("award","award");
-        }
-        List<WapUser> list = userMapper.findByZsId(userId);
-        model.addAttribute("list",list);
+    public String index(){
         return "agent/index";
+    }
+
+    @RequestMapping("/indexAjax")
+    @ResponseBody
+    public List indexAjax(HttpServletRequest request,Model model){
+        String name = SecurityContextHolder.getContext().getAuthentication().getName();
+        WapUser user  = userMapper.findByUsername(name);
+//        if(userId == null || userId == 0){
+//            String name = SecurityContextHolder.getContext().getAuthentication().getName();
+//            WapUser user  = userMapper.findByUsername(name);
+//            userId = user.getUserId();
+//            //发奖
+//            model.addAttribute("award","award");
+//        }
+        int page = Integer.parseInt(request.getParameter("page"));
+        int size = Integer.parseInt(request.getParameter("size"));
+        Sort sort = new Sort(Sort.Direction.DESC,"insertDate");
+        PageRequest pageable = PageRequest.of(page,size,sort);
+        Page<WapUser> list = userMapper.findByZsId(user.getUserId(),pageable);
+        return list.getContent();
     }
 
     /**
