e1548ecd169734b1be475a7e376b826689825216
baobye <baobye@sina.com>
2018-06-24 09:14:43 +0800

    实体增加对应字段注解

--- src/main/java/com/neusoft/baobye/ectouch/entity/StorageGoods.java
+++ src/main/java/com/neusoft/baobye/ectouch/entity/StorageGoods.java
@@ -1,5 +1,6 @@
 package com.neusoft.baobye.ectouch.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
@@ -7,6 +8,7 @@ import javax.persistence.Id;
 public class StorageGoods {
 
     @Id
+    @Column(name="STO_G_ID")
     private Long stoGId;
 
     private Long storageId;
