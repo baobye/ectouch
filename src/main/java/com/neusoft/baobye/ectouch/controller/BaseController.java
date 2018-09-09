ad87141ede21d06d79f156f38b659f077655276f
baobye <baobye@sina.com>
2018-09-09 10:49:29 +0800

    购物车部分 还没修改完

--- src/main/java/com/neusoft/baobye/ectouch/controller/BaseController.java
+++ src/main/java/com/neusoft/baobye/ectouch/controller/BaseController.java
@@ -1,9 +1,24 @@
 package com.neusoft.baobye.ectouch.controller;
 
+import com.neusoft.baobye.ectouch.entity.WapUser;
+import com.neusoft.baobye.ectouch.mapper.WapUserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 
 public abstract class BaseController {
+    @Autowired
+    private WapUserMapper userMapper;
     public String getUserName(){
-        return SecurityContextHolder.getContext().getAuthentication().getName();
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        UserDetails wapUser = (UserDetails) authentication.getPrincipal();
+        return wapUser.getUsername();
+    }
+
+    public Long getUserId(){
+        WapUser user  = userMapper.findByUsername(SecurityContextHolder.getContext().getAuthentication().getName());
+
+        return user.getUserId();
     }
 }
---
+++ src/main/java/com/neusoft/baobye/ectouch/entity/EchatEntity.java
@@ -0,0 +1,42 @@
+package com.neusoft.baobye.ectouch.entity;
+
+import java.util.List;
+
+public class EchatEntity {
+    private String title;
+    private List<String> legend;
+    private List<String> name;
+    private List<String> value;
+
+    public List<String> getName() {
+        return name;
+    }
+
+    public void setName(List<String> name) {
+        this.name = name;
+    }
+
+    public List<String> getValue() {
+        return value;
+    }
+
+    public void setValue(List<String> value) {
+        this.value = value;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public List<String> getLegend() {
+        return legend;
+    }
+
+    public void setLegend(List<String> legend) {
+        this.legend = legend;
+    }
+}
