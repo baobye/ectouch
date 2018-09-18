a35c3f22a7451e86de40b2f7391424b57dcfa07c
hyh <2442000@qq.com>
2018-09-18 18:01:33 +0800

    增加查看库房记录接口

---
+++ src/main/java/com/neusoft/baobye/ectouch/controller/AssetsExchangeController.java
@@ -0,0 +1,35 @@
+package com.neusoft.baobye.ectouch.controller;
+
+import com.neusoft.baobye.ectouch.mapper.AssetsExchangeMapper;
+import com.neusoft.baobye.ectouch.mapper.AssetsMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+@Controller
+@RequestMapping("/AssetsExchange")
+public class AssetsExchangeController {
+
+    @Autowired
+    private AssetsExchangeMapper mapper;
+
+    @RequestMapping("/record/{assetsId}")
+    public String indexAjax(@PathVariable Long assetsId, HttpServletRequest request,Model model) {
+        int page = Integer.parseInt(request.getParameter("page"));
+        int size = Integer.parseInt(request.getParameter("size"));
+        Sort sort = new Sort(Sort.Direction.DESC, "insertDate");
+        PageRequest pageable = PageRequest.of(page, size, sort);
+        Page list = mapper.findAssetsId(assetsId, pageable);
+        model.addAttribute("assets",list);
+        return "assets/assetsDetail";
+    }
+}
---
+++ src/main/java/com/neusoft/baobye/ectouch/mapper/AssetsExchangeMapper.java
@@ -0,0 +1,12 @@
+package com.neusoft.baobye.ectouch.mapper;
+
+import com.neusoft.baobye.ectouch.entity.AssetsExchange;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AssetsExchangeMapper extends JpaRepository<AssetsExchange,Long> {
+
+    Page<AssetsExchange> findAssetsId(Long assetsId,Pageable pageable);
+
+}
